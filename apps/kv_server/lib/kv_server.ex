d82d3f1927f0a6bb30981784ed8c6c8be4040ee1
Iggy <igoririanto@rocketmail.com>
2018-03-28 22:45:41 -0500

    modified kv_server

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,33 @@
+require Logger
+
 defmodule KVServer do
-  @moduledoc """
-  Documentation for KVServer.
-  """
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, 
+                      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
 
-  @doc """
-  Hello world.
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
 
-  ## Examples
+  defp serve(socket) do
+    socket 
+    |> read_line()
+    |> write_line(socket)
 
-      iex> KVServer.hello
-      :world
+    serve(socket)
+  end
 
-  """
-  def hello do
-    :world
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
